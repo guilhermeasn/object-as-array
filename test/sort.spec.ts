90b5e55dff3ca12ffc01062aabc28d9dc5355252
Guilherme Augusto <guilhermeasn@yahoo.com.br>
2023-05-07 20:52:53 -0300

    sort tests

---
+++ test/sort.spec.ts
@@ -0,0 +1,46 @@
+import sort from "../src/sort";
+import { roman, square } from "./data";
+
+describe('Sort Test', () => {
+
+    test('Testing key sort: Roman numerals in alphabetical order', () => {
+
+        expect(JSON.stringify(sort(roman, 'keys', (a, b) => a > b ? 1 : -1))).toBe(JSON.stringify({
+            C : 100,
+            D : 500,
+            I : 1,
+            L : 50,
+            M : 1000,
+            V : 5,
+            X : 10
+        }));
+
+    });
+
+    test('Testing value sort: Roman numerals in descending order', () => {
+
+        expect(JSON.stringify(sort(roman, 'values', (a, b) => b - a))).toBe(JSON.stringify({
+            M : 1000,
+            D : 500,
+            C : 100,
+            L : 50,
+            X : 10,
+            V : 5,
+            I : 1
+        }));
+
+    });
+
+    test('Testing entries sort: Biggest difference between the numbers', () => {
+
+        expect(JSON.stringify(sort(square, 'entries', ([ka, va], [kb, vb]) => (parseInt(ka) - va) - (parseInt(kb) - vb) || vb - va))).toBe(JSON.stringify({
+            '16' : 256,
+            '04' : 16,
+            '02' : 4,
+            '01' : 1,
+            '00' : 0
+        }));
+
+    });
+
+});
