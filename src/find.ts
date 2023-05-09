907442423b7d108e6344867a5ddb344b19e2ed7e
Guilherme Augusto <guilhermeasn@yahoo.com.br>
2023-05-09 11:55:39 -0300

    improved ident

--- src/find.ts
+++ src/find.ts
@@ -5,9 +5,11 @@ export type FindHandler<O extends object> = (value : O[keyof O], key : keyof O) 
 export default function find<O extends object, T extends keyof ObjectInfo<O>>(object : O, dataType : T, handler : FindHandler<O>) : ObjectInfo<O>[T] | null {
     for(let key in object) {
         if(handler(object[key], key)) {
-            return (dataType === 'key' ? key :
-                   dataType === 'value' ? object[key] :
-                   [key, object[key]]) as ObjectInfo<O>[T]
+            return (
+                dataType === 'key' ? key :
+                dataType === 'value' ? object[key] :
+                [key, object[key]]
+            ) as ObjectInfo<O>[T]
         }
     }
     return null;
