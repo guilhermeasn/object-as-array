919fef7ba6e3dd5d7e14eb6c009bbf7d1ac4474f
Guilherme Augusto <guilhermeasn@yahoo.com.br>
2023-05-08 23:25:25 -0300

    adjust reduce

--- src/reduce.ts
+++ src/reduce.ts
@@ -1,6 +1,6 @@
 export type ReduceHandler<O extends object, R> = (previousValue: R, currentValue: O[keyof O], currentKey : keyof O) => R
 
-export default function reduce<O extends object, R = O[keyof O]>(object : O, handler : ReduceHandler<O, R>, initial ?: R) : R | undefined {
+export default function reduce<O extends object, R = O[keyof O]>(object : O, handler : ReduceHandler<O, R>, initial ?: R) : R {
 
     let r : R | undefined = initial;
 
@@ -9,6 +9,6 @@ export default function reduce<O extends object, R = O[keyof O]>(object : O, han
         else r = handler(r as R, object[key], key);
     }
 
-    return r;
+    return <R>r;
 
 }
