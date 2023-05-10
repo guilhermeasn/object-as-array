1f955d134be10d4406647390d9af9d721f7b62a2
Guilherme Neves <guilhermeasn@yahoo.com.br>
2023-05-10 15:12:48 +0000

    commented funcs

--- src/toString.ts
+++ src/toString.ts
@@ -1,8 +1,8 @@
 /**
  * Turn object into a string
- * @param object 
- * @param expand 
- * @returns 
+ * @param object Target object
+ * @param expand Add line breaks and indentation
+ * @returns Resulting string
  */
 export default function toString(object : object, expand : boolean | number = false) : string {
     return JSON.stringify(object, undefined, typeof expand === 'number' ? expand : !!expand ? 2 : undefined);
--- src/valueOf.ts
+++ src/valueOf.ts
@@ -1,3 +1,9 @@
+/**
+ * Returns the value of an element of an object
+ * @param object Target object
+ * @param key Element key
+ * @returns Element value
+ */
 export default function valueOf<O extends object, K extends keyof O>(object : O, key : K) : O[K] {
     return object?.[key];
 }
