885ef65eaf1ad74d842802baef6caf5fe2f45b2a
Guilherme Augusto <guilhermeasn@yahoo.com.br>
2023-05-11 21:49:33 -0300

    Revert "build"
    This reverts commit 50d683b8a05bf9036850d73a594c5b7050a7f4e0.

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -14,7 +14,7 @@ const config = {
   url: 'https://guilhermeasn.github.io/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/object-as-array/',
+  baseUrl: 'object-as-array/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
