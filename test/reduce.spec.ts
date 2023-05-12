2937607fc01a2c2fa68fe6a32410bde1993557bb
Guilherme Neves <guilhermeasn@yahoo.com.br>
2023-05-12 08:35:31 -0300

    Update reduce.spec.ts

--- test/reduce.spec.ts
+++ test/reduce.spec.ts
@@ -3,13 +3,13 @@ import { roman } from "./data";
 
 describe('Reduce Test', () => {
 
-    test('Some romam numbers without init value', () => {
+    test('Sum romam numbers without init value', () => {
 
         expect(reduce(roman, (p, v) => p + v)).toBe(1666);
 
     });
 
-    test('Some romam numbers with init value', () => {
+    test('Sum romam numbers with init value', () => {
 
         expect(reduce(roman, (p, v) => p + v, 334)).toBe(2000);
 
