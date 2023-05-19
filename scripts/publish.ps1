a4958984f03cbf120455d3218963a23f0ae802f1
Guilherme Augusto <guilhermeasn@yahoo.com.br>
2023-05-18 21:18:02 -0300

    protected publish scripts

--- scripts/publish.ps1
+++ scripts/publish.ps1
@@ -1,4 +1,14 @@
-yarn build:ps
-Set-Location ./dist
-npm pack --pack-destination ..\\packs\\
-npm publish
+yarn test
+
+if($LASTEXITCODE -eq 0) {
+    
+    yarn build:ps
+    Set-Location ./dist
+    npm pack --pack-destination ..\\packs\\
+    npm publish
+
+} else {
+
+    Write-Output 'Build aborted because tests failed.'
+
+}
--- scripts/publish.sh
+++ scripts/publish.sh
@@ -1,4 +1,12 @@
-yarn build:sh
-cd ./dist
-npm pack --pack-destination ..\\packs\\
-npm publish
+if (yarn test); then
+
+    yarn build:sh
+    cd ./dist
+    npm pack --pack-destination ..\\packs\\
+    npm publish
+
+else
+
+    echo 'Build aborted because tests failed.'
+
+fi
